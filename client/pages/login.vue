265f84e04acca1ba78be9149d588d3926ed3c232
Syitechdev <dev@syitech-group.tech>
2025-03-03 20:19:17 +0000

    design de page login terminé

---
+++ client/pages/login.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+const form = reactive({ email: "", password: "" })
+</script>
+
+<template>
+  <div class="flex h-screen flex-col justify-center px-6 py-12 lg:px-8">
+    <el-card class="sm:mx-auto sm:w-full sm:max-w-sm" shadow="always">
+      <img class="w-[9vw] sm:w-[12vw] mx-auto" src="/public/logo-lawry.png" alt="lawry logo"></img>
+      <el-form class="space-y-6">
+        <el-form-item class="w-full" label-position="top">
+          <template #label>
+            <b class="text-red-800">Adresse Email</b>
+          </template>
+          <template #default>
+            <el-input v-model="form.email" />
+          </template>
+        </el-form-item>
+
+        <el-form-item class="w-full" label-position="top">
+          <template #label>
+            <b class="text-red-800">Mot de passe</b>
+          </template>
+          <template #default>
+            <el-input show-password type="password" v-model="form.password" />
+          </template>
+        </el-form-item>
+
+        <div class="flex flex-col gap-2">
+          <a href="#" class="font-semibold text-sm text-slate-900 hover:text-slate-600 text-center">Mot de passe
+            oublié?</a>
+          <button type="submit" class="flex w-full justify-center rounded-md bg-red-800 px-3 py-1.5 text-sm/6 font-semibold
+           text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">
+            Connexion
+          </button>
+        </div>
+      </el-form>
+      <p class="mt-10 text-center text-sm/6 text-gray-500">
+        êtes-vous membre?
+        <a href="#" class="font-semibold text-slate-900 hover:text-slate-700">Creer un compte ici</a>
+      </p>
+    </el-card>
+  </div>
+</template>
+
+<style scoped></style>
