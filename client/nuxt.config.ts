e0ad339ee8979c004a3a6ae069736caa0a380a0e
Syitechdev <dev@syitech-group.tech>
2025-03-05 17:04:42 +0000

    mode spa activé sur le front

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -3,6 +3,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  ssr: false,
   modules: [
     "@element-plus/nuxt",
     "@nuxtjs/tailwindcss",
